053054bf0849af0c0ef4550f51ec821efe1a6541
DA-PC-0473\admin <covj12@gmail.com>
2021-08-25 14:11:00 +0900

    파일탐색기 업로드 기능 구현

--- pages/api/explorer/index.js
+++ pages/api/explorer/index.js
@@ -1,30 +1,67 @@
-let Client = require('ssh2-sftp-client');
-let client = new Client();
+import formidable from "formidable";
+import fs from "fs";
+import SshClient from "../../../utils/SshClient";
 
-async function explorer(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json')
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+// WRITE FILE
+const writeFile = async (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.parse(req, async function (err, fields, files) {
     try {
-        client.connect({
-            host: '127.0.0.1',
-            port: '50000',
-            username: 'ysban',
-            password: '1234'
-          }).then(() => {
-            return client.append(req.body, '/abc.txt');
-          }).then((data) => {
-            return client.end();
-          }).catch(err => {
-            console.log(err, 'catch error');
-          })
-    } catch (error) {
-        console.error(error);
-        res.send({
-            status: "error",
-            message: "에러가 발생하였습니다.",
-            error: JSON.stringify(error)
-        })
+      await saveFile(files.file);
+    } catch (e) {
+      console.log(e);
     }
-}
+    return res.status(201).send("");
+  });
+};
 
-export default explorer
+// LOCAL TO SERVER
+const saveFile = async (file) => {
+  console.log("## SET LOCAL TO SERVER ##");
+  const data = fs.readFileSync(file.path);
+  try {
+    fs.writeFileSync(`./public/tempFiles/${file.name}`, data);
+    await fs.unlinkSync(file.path);
+    return;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// SERVER TO REMOTE
+const uploadToRemote = async (req, res) => {
+  console.log("### UPLOAD SERVER TO REMOTE ###");
+  const filename = req.__NEXT_INIT_QUERY["filename"];
+  const path = req.__NEXT_INIT_QUERY["path"] + "/";
+  const downUrl = "http://192.168.0.144:3000/tempFiles/";
+
+  const sshClient = new SshClient("127.0.0.1", "50000", "ysban", "1234");
+  console.log("URL -> curl " + downUrl + filename + " > " + path + filename);
+
+  try {
+      const msg = await sshClient.exec("curl " + downUrl + filename + " > " + path + filename, {})
+      await fs.rmSync(`./public/tempFiles/` + req.__NEXT_INIT_QUERY["filename"]);
+      console.log(msg);
+      return res.send({status: "success", data: msg.join("")})
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export default (req, res) => {
+  req.method === "POST"
+    ? writeFile(req, res)
+    : req.method === "PUT"
+    ? console.log("PUT")
+    : req.method === "DELETE"
+    ? console.log("DELETE")
+    : req.method === "GET"
+    ? uploadToRemote(req, res)
+    : res.status(404).send("");
+};
