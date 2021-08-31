48e75ea7ea204678d9190c85712a06d7efc71c52
DA-PC-0473\admin <covj12@gmail.com>
2021-08-31 09:31:03 +0900

    explorer UI 개발

--- pages/api/servers/[id]/explorer.js
+++ pages/api/servers/[id]/explorer.js
@@ -1,9 +1,9 @@
 import formidable from "formidable";
 import fs from "fs";
 import SshClient from "../../../../utils/SshClient";
-import ServerService from "../../../../services/ServerService"
-import { withSession } from 'next-session';
-import FileService from "../../../../services/FileService"
+import ServerService from "../../../../services/ServerService";
+import { withSession } from "next-session";
+import FileService from "../../../../services/FileService";
 
 export const config = {
   api: {
@@ -12,24 +12,25 @@ export const config = {
 };
 
 function getRandomUuid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 }
 
-const insertFileDb = async(userId, file, uuid, type, phase) => {
+const insertFileDb = async (userId, file, uuid, type, phase) => {
   return await FileService.newFile({
-    userId : userId,
-    fileName : file.name,
-    initTime : new Date(),
-    fileSize : file.size,
-    phase : phase, // L 로컬, F 운영관리 플랫폼, R 원격지
-    type : type,
-    fileKey : uuid,
-    checkTime : new Date()
-  })
-}
+    userId: userId,
+    fileName: file.name,
+    initTime: new Date(),
+    fileSize: file.size,
+    phase: phase, // L 로컬, F 운영관리 플랫폼, R 원격지
+    type: type,
+    fileKey: uuid,
+    checkTime: new Date(),
+  });
+};
 
 const downUrl = "http://localhost:3355/tempFiles/";
 
@@ -37,9 +38,9 @@ var process_cmd = (id, processType, filename, path, filekey) => {
   const uploadUrl = `http://localhost:3355/api/servers/${id}/explorer`;
   if (processType === "upload") {
     var enc = encodeURI(`${downUrl}${filekey}`);
-    return `curl "${enc}" > "${path+filename}"`;
+    return `curl "${enc}" > "${path + filename}"`;
   } else if (processType === "download") {
-    return `curl -F "file=@${path+filename}" ${uploadUrl}?type="download" && rm -rf "${path+filename}"`
+    return `curl -F "file=@${path + filename}" ${uploadUrl}?type="download"`;
   }
 };
 
@@ -50,7 +51,7 @@ const deleteFile = async (req, res) => {
 };
 
 // WRITE FILE
-const writeFile = async (req, res, userId) => {  
+const writeFile = async (req, res, userId) => {
   const form = new formidable.IncomingForm();
   form.parse(req, async function (err, fields, files) {
     try {
@@ -59,7 +60,11 @@ const writeFile = async (req, res, userId) => {
     } catch (e) {
       console.log(e);
     }
-    return res.json({status: '201', fileKey: uuid, fileName: files.file.name});
+    return res.json({
+      status: "201",
+      fileKey: uuid,
+      fileName: files.file.name,
+    });
   });
 };
 
@@ -68,16 +73,18 @@ const saveFile = async (file, uuid, type, userId) => {
   console.log("## SAVE AT SERVER ##");
   const data = fs.readFileSync(file.path);
   try {
-    if(type === "upload"){
+    if (type === "upload") {
       fs.writeFileSync(`./public/tempFiles/${uuid}`, data);
-      await fs.unlinkSync(file.path);      
+      await fs.unlinkSync(file.path);
       // DB에 파일내용 저장
       var file = await insertFileDb(userId, file, uuid, type, "F");
-      console.log('file : ' + JSON.stringify(file));
       return;
-    } else if(type === "download") {
+    } else if (type === "download") {
       fs.mkdirSync(`./public/tempFiles/${uuid}`);
-      fs.writeFileSync(`./public/tempFiles/${uuid}/${decodeURI(file.name)}`, data);
+      fs.writeFileSync(
+        `./public/tempFiles/${uuid}/${decodeURI(file.name)}`,
+        data
+      );
       await fs.unlinkSync(file.path);
       return;
     }
@@ -88,23 +95,36 @@ const saveFile = async (file, uuid, type, userId) => {
 
 // SERVER TO REMOTE
 const processToRemote = async (req, res) => {
-  let server = await ServerService.findServerById(req.query['id']);
-  const sshClient = new SshClient(server.ip, server.port, server.user, server.password);
+  let server = await ServerService.findServerById(req.query["id"]);
+  const sshClient = new SshClient(
+    server.ip,
+    server.port,
+    server.user,
+    server.password
+  );
   console.log("## PROCESS TO REMOTE ##");
   try {
     var result;
-    const msg = await sshClient.exec(
-      process_cmd(req.query['id'], req.query["type"], req.query["filename"], req.query["path"] + "/", req.query["filekey"]),
-      {}
-    ).then(res => {
-      res.forEach(ele => {
-        if(ele.indexOf("fileKey") != -1){
-          result = ele
-        }
-      });
+    const msg = await sshClient
+      .exec(
+        process_cmd(
+          req.query["id"],
+          req.query["type"],
+          req.query["filename"],
+          req.query["path"] + "/",
+          req.query["filekey"]
+        ),
+        {}
+      )
+      .then((res) => {
+        res.forEach((ele) => {
+          if (ele.indexOf("fileKey") != -1) {
+            result = ele;
+          }
+        });
 
-      return res;
-    })
+        return res;
+      });
 
     if (req.query["type"] === "upload") {
       await fs.rmSync(`./public/tempFiles/` + req.query["filekey"]);
@@ -117,34 +137,55 @@ const processToRemote = async (req, res) => {
 };
 
 const controlData = async (req, res, userId) => {
-  if(req.query["type"] === "search"){
+  if (req.query["type"] === "search") {
     res.send({
       status: "success",
-      fileList: await FileService.findFiles(userId)
-    }); 
-  } else if(req.query["type"] === "update"){
+      fileList: await FileService.findFiles(userId),
+    });
+  } else if (req.query["type"] === "update") {
     res.send({
       status: "success",
-      fileList: await FileService.updateFiles(userId)
-    }); 
-  } else if(req.query["type"] === "remove"){
+      fileList: await FileService.updateFiles(userId),
+    });
+  } else if (req.query["type"] === "remove") {
     await FileService.removeFiles(userId);
     res.send({
-      status: "success"
-    }); 
+      status: "success",
+    });
+  }
+};
+
+const excuteCmd = async (req, res) => {
+  let server = await ServerService.findServerById(req.query["id"]);
+  const sshClient = new SshClient(
+    server.ip,
+    server.port,
+    server.user,
+    server.password
+  );
+
+  try {
+    const cmd = await sshClient.exec(req.query["cmd"], {}).then((res) => {
+      console.log(res);
+      return res;
+    });
+
+    return res.send(result);
+  } catch (e) {
+    console.log(e);
   }
 };
 
 export default withSession(async (req, res) => {
   // 원격에서 curl로 들어올때 세션처리...??
   var userId = "";
-  if(req.session.auth !== undefined){
-    userId = req.session.auth.user.userId
+  if (req.session.auth !== undefined) {
+    userId = req.session.auth.user.userId;
   }
   req.method === "POST"
     ? writeFile(req, res, userId)
     : req.method === "PUT"
-    ? controlData(req, res, userId)
+    ? excuteCmd(req, res)
     : req.method === "DELETE"
     ? deleteFile(req, res)
     : req.method === "GET"
--- pages/servers/[id]/explorer.js
+++ pages/servers/[id]/explorer.js
@@ -1,134 +1,231 @@
 import React, { useState } from "react";
+import Box from "@material-ui/core/Box";
+import ListItem from "@material-ui/core/ListItem";
+import { Paper, Grid, Divider, List, InputBase, IconButton } from "@material-ui/core";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableRow from "@material-ui/core/TableRow";
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepButton from '@material-ui/core/StepButton';
+import { makeStyles } from '@material-ui/core/styles';
+import DownloadButton from '@material-ui/icons/GetApp';
+import UploadButton from '@material-ui/icons/Publish';
+import InputIcon from '@material-ui/icons/Input';
 
-const ServerExplorer = () => {
-  const [file, setFile] = useState(null);
-  const [fileRow, setFileRow] = useState([]); 
-  const [server, setServer] = React.useState({});
-  const apiUrl = `/api/servers/${server["id"]}/explorer`;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  backButton: {
+    marginRight: theme.spacing(1),
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+  inputRoot: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}));
+
+function getSteps() {
+  return ['Local', 'Server', 'Remote'];
+}
+
+const ServerExplorer = () => {  
+  const classes = useStyles();  
+  const steps = getSteps();
+
+  const [currentPath, setCurrentPath] = useState("/home/danawa");
+  const [cmd, setCmd] = useState("ls -al");
+  const [dirData, setDirdata] = useState();
 
   React.useEffect(() => {
-    const url = "/api" + location.pathname.replace("/explorer", "");
+    var url = "/api" + location.pathname.replace("/explorer", "");
     fetch(url)
       .then((res) => res.json())
       .then((body) => {
         console.log(body["server"]);
         setServer(body["server"]);
       });
-  }, []);
 
-  if (Object.keys(server).length < 3) {
-    return null;
-  }
-
-  const uploadToClient = (event) => {
-    if (event.target.files && event.target.files[0]) {
-      const i = event.target.files[0];
-      setFile(i);
-    }
-  };
-
-  const uploadToRemote = async (fileKey, fileName) => {
-    const path = "/home/danawa/apps/explorer";
-    await fetch(
-      apiUrl +
-        `?type=upload&&filekey=${fileKey}&&filename=${fileName}&&path=${path}`,
-      {
-        method: "GET",
-      }
-    )
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((error) => console.error("Error:", error));
-  };
-
-  // 파일 업로드
-  const upload = async () => {
-    const body = new FormData();
-    body.append("file", file);
-
-    // 로컬 -> 서버 파일 업로드
-    await fetch(apiUrl + "?type=upload", {
-      method: "POST",
-      body,
-    })
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        console.log(data);
-        if (data.status === "201") {
-          // 서버 -> 원격 파일 업로드
-          uploadToRemote(data.fileKey, data.fileName);
-        }
-      })
-      .catch((error) => console.error("Error:", error));
-  };
-
-  const tableCRUD = (e) => {
-     fetch(apiUrl + `?type=${e.target.value}` , {
+    fetch(apiUrl + `?cmd=${cmd}&&currentPath=${currentPath}`, {
       method: "PUT",
     })
-      .then((res) => { 
-        return res.json();      
+      .then((res) => {
+        return res.json();
       })
       .then((res) => {
-        debugger
+        debugger;
       })
       .catch((error) => console.error("Error:", error));
-  }
-
-  const download = async () => {
-    const filename = file.name;
-    const path = "/home/danawa/apps/explorer";
-
-    await fetch(apiUrl + `?type=download&&filename=${filename}&&path=${path}`, {
-      method: "GET",
-    })
-      .then((res) => {  
-        return res.json();      
-      })
-      .then((res) => {
-        const a = document.createElement("a");
-        a.href = `http://localhost:3355/tempFiles/${res.fileKey}/${res.fileName}`
-        a.download = file.name;
-        a.click();
-        a.remove();
-        // setTimeout(() => {
-        //   fetch(
-        //       "/api/servers/explorer?filename=" + filename,
-        //       {
-        //         method: "DELETE",
-        //       }
-        //     )
-        // });
-      })
-      .catch((error) => console.error("Error:", error));
-  };
-
-  
+  }, []);
 
   return (
     <div>
-      <div>
-        <h4>Select file</h4>
-        <input type="file" name="myFile" onChange={uploadToClient} />
-        <button className="btn btn-primary" type="submit" onClick={upload}>
-          Upload
-        </button>
-        <button className="btn btn-primary" type="submit" onClick={download}>
-          Download
-        </button>
-        <button className="btn btn-primary" type="submit" onClick={tableCRUD} value="search">
-          Search
-        </button>
-        <button className="btn btn-primary" type="submit" onClick={tableCRUD} value="update">
-          Update
-        </button>
-        <button className="btn btn-primary" type="submit" onClick={tableCRUD} value="remove">
-          Remove
-        </button>
-      </div>
+      <Box >
+        <Paper elevation={2} >
+            <Grid container>
+                <Grid item xs={12}>
+                    <Paper component="form" className={classes.inputRoot}>
+        
+                        <InputBase
+                            className={classes.input}
+                            placeholder="ls -al"
+                            fullWidth
+                        />
+                        
+                        <Divider className={classes.divider} orientation="vertical" />
+                        <IconButton color="primary" className={classes.iconButton} aria-label="directions">
+                            <InputIcon />
+                        </IconButton>
+                    </Paper>
+                </Grid>
+            </Grid>
+          <Divider />
+          <Grid container alignItems="center">
+            <List
+              dense={true}
+              style={{
+                overflow: "scroll",
+                height: 500,
+                maxHeight: 500,
+                width: "100%",
+                backgroundColor: "black",
+                color: "white",
+                font: "sans-serif",
+                fontSize: "16px",
+                overflowX: "hidden"
+              }}
+            >
+              <ListItem>
+                drwx------. 19 danawa users 4096 8월 26 14:05 .
+                <br />
+                drwxr-xr-x. 11 root root 144 7월 1 16:12 ..
+                <br />
+                -rw------- 1 danawa users 258506 8월 30 09:31 .bash_history
+                <br />
+                -rw-r--r--. 1 danawa users 18 8월 8 2019 .bash_logout
+                <br />
+                -rw-r--r--. 1 danawa users 193 8월 8 2019 .bash_profile
+                <br />
+                -rw-r--r-- 1 danawa users 259 4월 6 2020 .bashrc
+                <br />
+                drwxr-xr-x 3 danawa users 22 5월 10 14:16 .cache
+                <br />
+                drwxr-xr-x 9 danawa users 122 12월 16 2020 .gradle
+                <br />
+                drwxr-xr-x 3 danawa users 24 12월 7 2020 .java
+                <br />
+                drwxr-xr-x 4 danawa users 39 2월 1 2021 .m2
+                <br />
+                drwxr-xr-x 122 danawa users 4096 5월 31 15:21 .npm
+                <br />
+                drwxr----- 3 danawa users 19 5월 20 2020 .pki
+                <br />
+                drwx------ 2 danawa users 48 10월 16 2020 .ssh
+                <br />
+                drwxr-xr-x 2 danawa users 24 7월 23 12:58 .vim
+                <br />
+                -rw------- 1 danawa users 14049 8월 26 14:05 .viminfo
+                <br />
+                -rw------- 1 danawa users 0 4월 6 2020 .viminfo.tmp
+                <br />
+                -rw------- 1 danawa users 0 6월 18 2020 .viminfz.tmp
+                <br />
+                drwxr-xr-x 4 danawa users 47 5월 10 14:13 Application
+                <br />
+                drwxr-xr-x 38 danawa users 4096 8월 27 17:57 apps
+                <br />
+                drwxr-xr-x 5 danawa users 72 8월 18 17:18 backup
+                <br />
+                drwxr-xr-x 2 danawa users 37 8월 6 08:59 cache
+                <br />
+                -rw-r--r-- 1 danawa users 0 1월 25 2021 crontab.log
+                <br />
+                drwxr-xr-x 3 danawa users 17 5월 10 14:16 go
+                <br />
+                -rw-r--r-- 1 danawa users 73 5월 28 13:59 java.Polic
+                <br />
+                -rwxrwxrwx 1 danawa users 2301 7월 28 2020 jvm.options
+                <br />
+                drwxr-xr-x 3 danawa users 17 12월 30 2020 lib
+                <br />
+                -rwxrwxrwx 1 danawa users 7921 8월 20 09:29 log4j2.properties
+                <br />
+                drwxr-xr-x 3 danawa users 62 6월 1 17:15 redis
+                <br />
+                drwxr-xr-x 4 danawa users 72 5월 4 16:10 scrap
+                <br />
+                drwxr-xr-x 2 danawa users 24 8월 11 16:13 tmp
+              </ListItem>
+            </List>
+            <TableContainer
+              component={Paper}
+              style={{
+                overflow: "scroll",
+                height: 380,
+                maxHeight: 420,
+                width: "100%",
+                overflowX: "hidden",
+              }}
+            >
+              <Table>              
+                <TableBody>
+                  {
+                    <TableRow>
+                      <TableCell component="th" scope="row" style={{width: "80px",  backgroundColor:"#A4C4EE", borderRight: "1px solid silver", textAlign: "center"}}>
+                          <UploadButton fontSize="large" />
+                      </TableCell>
+                      <TableCell component="th" scope="row" style={{minWidth:100}}>
+                        /temp/Docker-compose.yml
+                      </TableCell>
+                      <TableCell align="left" style={{minWidth:250}}>
+                      <Stepper style={{margin:"0", padding:"0"}}  alternativeLabel>
+                        {steps.map((label, index) => {
+                            const stepProps = {};
+                            return (
+                                <Step key={label} {...stepProps}>
+                                    <StepButton                                      
+                                        disabled
+                                        completed={index === 0 || index === 1 ? true : false}
+                                    >
+                                    {label}
+                                </StepButton>
+                                </Step>
+                            );
+                        })}
+                    </Stepper>
+                        <div align="center">
+                          시작 : 2021.08.30 09:40 소요시간 : 40분
+                        </div>
+                      </TableCell>
+                      <TableCell align="center" style={{minWidth:250}}>Uploading ...</TableCell>
+                    </TableRow>
+                  }
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        </Paper>
+      </Box>
     </div>
   );
 };
