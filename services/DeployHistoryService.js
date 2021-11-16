4e3bbc08224b3ac7208f2c379b7e38c6fd3bd926
DA-PC-0473\admin <covj12@gmail.com>
2021-11-16 22:35:51 +0900

    테이블 추가

--- services/DeployHistoryService.js
+++ services/DeployHistoryService.js
@@ -52,7 +52,13 @@ export default {
     }
   },
   findDeployHistory: async (groupId) => {
-    return await DeployHistory.findAll();
+    return await DeployHistory.findAll({
+      where: { groupId: groupId },
+      order: [
+        ["deployTime", "DESC"],
+        ["id", "DESC"],
+      ],
+    });
   },
   removeDeployHistory: async () => {
     await DeployHistory.destroy();
