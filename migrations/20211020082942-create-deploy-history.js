ffefd2cd1adff6e29a90720a01c1181aa188f44f
DA-PC-0473\admin <covj12@gmail.com>
2021-11-17 15:34:09 +0900

    히스토리 테이블 제거

--- migrations/20211020082942-create-deploy-history.js
+++ migrations/20211020082942-create-deploy-history.js
@@ -1,46 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('deploy_histories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      groupId: {
-        type : Sequelize.STRING
-      },
-      deployTime: {
-        type: Sequelize.DATE
-      },
-      user: {
-        type: Sequelize.STRING
-      },
-      result: {
-        type: Sequelize.STRING
-      },
-      service: {
-        type: Sequelize.STRING
-      },
-      deployId: { 
-        type : Sequelize.STRING 
-      },
-      deployEndTime: {
-        type : Sequelize.DATE
-      },
-      deployType: {
-        type : Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+    await queryInterface.dropTable('deploy_histories');
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('deploy_histories');
