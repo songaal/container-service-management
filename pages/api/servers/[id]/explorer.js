3a6d7169a45c38e38af20bb894f54da1c6090037
zozond <zozond@naver.com>
2022-11-25 15:41:23 +0900

    Update explorer.js

--- pages/api/servers/[id]/explorer.js
+++ pages/api/servers/[id]/explorer.js
@@ -173,7 +173,7 @@ const processToRemote = async (req, res, userId) => {
         
         return res;
       });
-    console.log(`checkExist >>>> ${JSON.stringify(req.body)}`)
+    console.log(`checkExist >>>> ${JSON.stringify(req)}`)
     if(req.query["type"] === "checkExist"){
       console.log(`await >>>>> cd ${req.query["path"]} && file ${req.query["filename"]}`);
       await sshClient.exec(`cd ${req.query["path"]} && file ${req.query["filename"]}` , {})
